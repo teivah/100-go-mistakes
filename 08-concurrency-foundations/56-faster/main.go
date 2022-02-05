ab22488bb071120c8d15ad796953602ef7d16cbb
teivah <t.harsanyi@thebeat.co>
2022-02-05 14:07:04 +0100

    Reviews

--- 08-concurrency-foundations/56-faster/main.go
+++ 08-concurrency-foundations/56-faster/main.go
@@ -1,18 +1,54 @@
 package main
 
-import "sync"
+import (
+	"sync"
+)
 
 func sequentialMergesort(s []int) {
-	if len(s) > 1 {
-		middle := len(s) / 2
-		sequentialMergesort(s[:middle])
-		sequentialMergesort(s[middle:])
-		merge(s, middle)
+	if len(s) <= 1 {
+		return
 	}
+
+	middle := len(s) / 2
+	sequentialMergesort(s[:middle])
+	sequentialMergesort(s[middle:])
+	merge(s, middle)
 }
 
 func parallelMergesortV1(s []int) {
-	if len(s) > 1 {
+	if len(s) <= 1 {
+		return
+	}
+
+	middle := len(s) / 2
+
+	var wg sync.WaitGroup
+	wg.Add(2)
+
+	go func() {
+		defer wg.Done()
+		parallelMergesortV1(s[:middle])
+	}()
+
+	go func() {
+		defer wg.Done()
+		parallelMergesortV1(s[middle:])
+	}()
+
+	wg.Wait()
+	merge(s, middle)
+}
+
+const max = 2048
+
+func parallelMergesortV2(s []int) {
+	if len(s) <= 1 {
+		return
+	}
+
+	if len(s) <= max {
+		sequentialMergesort(s)
+	} else {
 		middle := len(s) / 2
 
 		var wg sync.WaitGroup
@@ -20,44 +56,16 @@ func parallelMergesortV1(s []int) {
 
 		go func() {
 			defer wg.Done()
-			parallelMergesortV1(s[:middle])
+			parallelMergesortV2(s[:middle])
 		}()
 
 		go func() {
 			defer wg.Done()
-			parallelMergesortV1(s[middle:])
+			parallelMergesortV2(s[middle:])
 		}()
 
 		wg.Wait()
 		merge(s, middle)
-	}
-}
-
-const max = 2048
-
-func parallelMergesortV2(s []int) {
-	if len(s) > 1 {
-		if len(s) <= max {
-			sequentialMergesort(s)
-		} else {
-			middle := len(s) / 2
-
-			var wg sync.WaitGroup
-			wg.Add(2)
-
-			go func() {
-				defer wg.Done()
-				parallelMergesortV2(s[:middle])
-			}()
-
-			go func() {
-				defer wg.Done()
-				parallelMergesortV2(s[middle:])
-			}()
-
-			wg.Wait()
-			merge(s, middle)
-		}
 	}
 }
 
